8ff5202ee645b9e3e2b38c3e8685cea2e2ee35ee
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-29 17:45:48 +0000

    Featured: Wrap sidebar around app component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import Sidebar from '@/components/Sidebar'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+<Sidebar>
+
+<Component {...pageProps} />
+</Sidebar>
+  ) 
 }
