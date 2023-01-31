cd051d3f3a1acbd86cfaeed9c8e04677bd1be55b
owusuvictorjnr <owusuvictor80@gmail.com>
2023-01-31 20:05:39 +0000

    Feature: Import bar from chartjs.

--- components/BarChart.js
+++ components/BarChart.js
@@ -1,5 +1,39 @@
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
 const BarChart = () => {
-  return <div>BarChart</div>;
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
+
+  const [chartOptions, setChartOptions] = useState({});
+
+  useEffect(() => {}, []);
+  return (
+    <>
+      <div className="w-full md:col-span-2 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-[#ffffff] ">
+        <Bar data={chartData} options={chartOptions} />
+      </div>
+    </>
+  );
 };
 
 export default BarChart;
